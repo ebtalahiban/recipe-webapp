35e7c341197ab61efba63c3b797e4b07df834d62
Erica Talahiban <ebtalahiban@up.edu.ph>
2023-09-02 12:48:44 +0800

    Delete the outdated routes

--- server/routes/recipes.js
+++ server/routes/recipes.js
@@ -12,43 +12,4 @@ router.get('/categories', listCategories);
 router.get('/category/:name', filterByCategory);
 router.get('/meal/:id', fetchRecipeByID);
 
-// List all recipe categories
-router.get('/categories', async (req, res) => {
-    try {
-        const response = await axios.get('https://www.themealdb.com/api/json/v1/1/categories.php');
-        const categories = response.data.categories;
-        res.status(200).json(categories);
-    } catch (error) {
-        console.error('Error listing categories:', error);
-        res.status(500).json({ error: 'Unable to list categories' });
-    }
-});
-
-// List all recipes in a category
-router.get('/category/:name', async (req, res) => {
-    const name = req.params.name;
-    try {
-        const response = await axios.get(`https://www.themealdb.com/api/json/v1/1/filter.php?c=${name}`);
-        const meals = response.data.meals;
-        res.status(200).json(meals);
-    } catch (error) {
-        console.error('Error listing categories:', error);
-        res.status(500).json({ error: 'Unable to list categories' });
-    }
-});
-
-// Fetch full recipe details of a meal
-router.get('/meal/:id', async (req, res) => {
-    const id = req.params.id;
-    try {
-        const response = await axios.get(`https://www.themealdb.com/api/json/v1/1/lookup.php?i=${id}`);
-        const recipe = response.data.meals;
-        console.log(recipe);
-        res.status(200).json(recipe);
-    } catch (error) {
-        console.error('Error searching for meals: ', error);
-        res.status(500).json({ error: 'Unable to search meals' });
-    }
-});
-
 export default router;
